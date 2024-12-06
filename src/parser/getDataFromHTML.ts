500286c85a3076001f038c5419bbd45c3ad5fbc5
Jan Krivohlavek <krajvy@gmail.com>
2024-12-06 22:41:01 +0100

    feat: Parse and convert MTBIKER clanky page

--- src/parser/getDataFromHTML.ts
+++ src/parser/getDataFromHTML.ts
@@ -1,4 +1,5 @@
 import { parseDataFromKameniceZpravodaj } from './parseDataFromKameniceZpravodaj.js';
+import { parseDataFromMtbikerClanky } from './parseDataFromMtbikerClanky.js';
 import { parseDataFromSkolaSeberovBlog } from './parseDataFromSkolaSeberovBlog.js';
 import { parseDataFromSkolaSeberovPatecniPozdrav } from './parseDataFromSkolaSeberovPatecniPozdrav.js';
 
@@ -12,6 +13,9 @@ export const getDataFromHTML = (record: UrlRow, source: string): Feed => {
       break;
     case 'kamenice-zpravodaj':
       return parseDataFromKameniceZpravodaj(record, source);
+      break;
+    case 'mtbiker-clanky':
+      return parseDataFromMtbikerClanky(record, source);
       break;
     default:
       break;
