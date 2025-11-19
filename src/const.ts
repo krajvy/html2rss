61111713e452b19e2af285041c068037707f7f42
Jan Krivohlavek <krajvy@gmail.com>
2025-11-19 11:15:33 +0100

    fix: Process at least some success feeds when some other fails

--- src/const.ts
+++ src/const.ts
@@ -1,3 +1,6 @@
+const USER_AGENT =
+  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3';
+
 const URLS: UrlRow[] = [
   {
     title: 'Škola Šeberov - Páteční pozdrav',
@@ -21,4 +24,4 @@ const URLS: UrlRow[] = [
   },
 ];
 
-export { URLS };
+export { USER_AGENT, URLS };
